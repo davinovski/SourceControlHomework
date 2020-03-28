c7337532a0364f5cd069ada6990184ecaf4e4141
hdavinovski <hdavinovski@EN310181>
2020-03-28 18:55:56 +0100

    changed printing format in program

--- SourceControlHomework/Program.cs
+++ SourceControlHomework/Program.cs
@@ -8,6 +8,7 @@ namespace SourceControlHomework
         {
             City NewYork = new City("New York", 8.6, "USA");
              NewYork.PrintCityMethod();
+            Console.WriteLine("New York is the biggest city in the United States of America.");
 
             Console.ReadKey();
         }
