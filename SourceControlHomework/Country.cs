dcc1eb9a70a06c4c3d8776a6fa4e2a864a271b7c
hdavinovski <hdavinovski@EN310181>
2020-03-28 19:08:42 +0100

    added country class

---
+++ SourceControlHomework/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SourceControlHomework
+{
+    class Country
+    {
+        public string Name { get; set; }
+        public string Continent { get; set; }
+
+        public Country(string name, string continent)
+        {
+            Name = name;
+            Continent = continent;
+        }
+
+        public void printCountryAndContinent()
+        {
+            Console.WriteLine(this.Name + " is located in the continent of " + this.Continent);
+        }
+    }
+}
--- SourceControlHomework/Program.cs
+++ SourceControlHomework/Program.cs
@@ -7,9 +7,10 @@ namespace SourceControlHomework
         static void Main(string[] args)
         {
             City NewYork = new City("New York", 8.6, "USA");
-             NewYork.PrintCityMethod();
+            NewYork.PrintCityMethod();
             Console.WriteLine("New York is the biggest city in the United States of America.");
-
+            Country USA = new Country("USA", "North America");
+            USA.printCountryAndContinent();
             Console.ReadKey();
         }
     }
